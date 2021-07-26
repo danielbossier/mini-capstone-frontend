5580346cb9393d65d61dd54850d845007ee1ffe6
danielbossier <dbossier21@gmail.com>
2021-07-26 12:58:39 -0700

    added index method

--- src/views/ProductsIndex.vue
+++ src/views/ProductsIndex.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="products-index">
-    <div v-for="product in products" :key="product.id"></div>
+    <h1>All Products</h1>
+    <div v-for="product in products" :key="product.id">
+      <h2>Name: {{ product.name }}</h2>
+      <p>Description: {{ product.description }}</p>
+      <img v-bind:src="product.image_url" alt="product.title" />
+    </div>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      products: [],
+    };
+  },
+  created: function () {
+    this.indexProducts();
+  },
+  methods: {
+    indexProducts: function () {
+      axios.get("/products").then((response) => {
+        this.products = response.data;
+        console.log("All products:", this.products);
+      });
+    },
+  },
+};
+</script>
